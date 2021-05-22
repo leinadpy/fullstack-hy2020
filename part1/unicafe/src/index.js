e25259ffb5ec82471f1a3bb0856ce82382e9cc12
Daniel Ojeda <leinadpy@gmail.com>
2021-05-22 11:46:05 -0400

    Ejercicio 1.10 terminado

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,11 +1,49 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Titulo = ({title}) => {
+const Titulo = ({ title }) => {
+  return <h2>{title}</h2>;
+};
+
+const Button = ({ text, setAction }) => {
+  const updateState = () => {
+    setAction((prevstate) => prevstate + 1);
+  };
+  return <button onClick={updateState}>{text}</button>;
+};
+
+const Statistic = ({ text, value }) => {
   return (
-    <h2>{title}</h2>
-  )
-}
+    <p>
+      {text} {value}
+    </p>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <>
+      {good === 0 && neutral === 0 && bad === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <div>
+          <Statistic text="good" value={good} />
+          <Statistic text="neutral" value={neutral} />
+          <Statistic text="bad" value={bad} />
+          <Statistic text="all" value={good + neutral + bad} />
+          <Statistic
+            text="average"
+            value={(good - bad) / (good + neutral + bad)}
+          />
+          <Statistic
+            text="positive"
+            value={(good / (good + neutral + bad)) * 100 + " %"}
+          />
+        </div>
+      )}
+    </>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
@@ -16,8 +54,13 @@ const App = () => {
   return (
     <div>
       <Titulo title="give feedback" />
-
+      <div>
+        <Button text="good" setAction={setGood} />
+        <Button text="neutral" setAction={setNeutral} />
+        <Button text="bad" setAction={setBad} />
+      </div>
       <Titulo title="statistics" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
